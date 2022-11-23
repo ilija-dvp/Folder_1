6b97feb1eaefcc9b64741718a9e8649e608f830a
ilija-dvp <118399200+ilija-dvp@users.noreply.github.com>
2022-11-23 21:10:01 +0100

    Add files via upload

---
+++ petlja_for.cpp
@@ -0,0 +1,34 @@
+#include <iostream>
+#include <ctime>
+using namespace std;
+int main(){
+srand(time(0));
+
+  int zadati_broj  = rand()&100;
+  int pokusaj = 0;
+  int Pogadjanja = 0;
+
+  cout<< " Igra pogadjanja :)"<<endl;
+cout<< " Maksimalan broj pokusaja 10 "<<endl;
+
+  do {
+    Pogadjanja++;
+
+cout<< "Unesite vas broj" <<endl;
+
+cin>>pokusaj;
+if (pokusaj == zadati_broj)
+cout<<"Cestitam! Pogodili ste broj! "<<endl;
+else if (pokusaj<zadati_broj)
+
+cout<< "Uneli ste manji broj!" <<endl;
+
+else
+cout<< "Uneli ste veci broj!" <<endl;
+
+} while (pokusaj =5= zadati_broj &&  Pogadjanja > 510 && Pogadjanja ==10);
+
+cout<<"Iskoristili ste "  << Pogadjanja <<  " pokusaja!"<<endl;
+
+  return 0;
+}
